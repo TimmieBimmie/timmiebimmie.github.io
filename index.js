4aca408f14112768213192c5e21ee0859db5a60c
timmie.bimmie <101521401+TimmieBimmie@users.noreply.github.com>
2024-04-08 22:42:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ document.addEventListener('contextmenu', event => {
 
 
 
-const links = ['img/isaiah_53-3.jpg', 'img/Matthew_28_6.jpg', 'img/Romans_6_23.jpg', 'img/Romans_8_39.jpg'];
+const links = ['img/isaiah_53-3.jpg', 'img/Matthew_28_6.jpg', 'img/Romans_6_23.jpg', 'img/Romans_8_39.jpg', 'img/Psalm_56_4.jpg',
+'img/Romans_8_35.jpg', 'img/Romans_8_28.jpg', 'img/Isaiah_53_5.jpg', 'img/Romans_15_13.jpg', 'img/Joshua_1_9.jpg', 'img/John_11_25-26.jpg',
+'img/Deutoronomy_31_8.jpg', 'img/Colossians_1_28.jpg', 'img/Ecclesiastes.jpg'];
 
 const container = document.getElementById("container");
 
@@ -26,13 +28,18 @@ var randImg2;
 function scheduleDailyRun() {
     const lastRun = localStorage.getItem('lastRun');
     const lastNum = localStorage.getItem('Number');
+    const usedArr = localStorage.getItem('Array');
+    const i = localStorage.getItem('i');
     const now = new Date();
   
-    if (!lastRun || new Date(lastRun).getDate() !== now.getDate() || !lastNum) {
+    if (!lastRun || new Date(lastRun).getDate() !== now.getDate() || !lastNum || !usedArr) {
       newVerse();
       localStorage.setItem('lastRun', now);
       localStorage.setItem('Number', randImg2);
+      localStorage.setItem('i', 0);
+      localStorage.setItem('Array', '');
     }
+
     const nextRun = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1);
     const delay = nextRun - now;
   
@@ -47,18 +54,32 @@ function scheduleDailyRun() {
   scheduleDailyRun();
 
 
-
-
-const lastImg = localStorage.getItem('Number');
+  const lastImg = localStorage.getItem('Number');
 img.src = links[lastImg];
 a.href = links[lastImg];
 a.download = links[lastImg].replace("img/", '');
 
+
+var i = localStorage.getItem('i');
+
+
 container.appendChild(img);
+var usedArr;
+
+if (usedArr.length() == links.length()){
+localStorage.clear();
+window.location.reload();
+}
+
 
 
 function newVerse() {
+    usedArr = JSON.parse(localStorage.getItem('Array') || "[]");
     var randImg = Math.floor(Math.random() * links.length);
+    do {
+      randImg = Math.floor(Math.random() * links.length);
+    }
+    while (usedArr.includes(randImg));
     randImg2 = randImg;
     a.className = "link";
     a.href = links[randImg];
@@ -67,6 +88,8 @@ function newVerse() {
     a.download = fileName;
     img.src = links[randImg];   
     container.appendChild(img);
+    usedArr.push(randImg);
+    localStorage.setItem('Array', JSON.stringify(usedArr));
 }
 
 
